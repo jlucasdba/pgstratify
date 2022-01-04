4b549982740144a7534621451be24ab38ec12037
James Lucas <73145226+jlucasdba@users.noreply.github.com>
2022-01-03 23:59:50 -0600

    Add release build script

---
+++ buildrelease.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# define these to cross-build for another arch/os
+#TARGETARCH=
+#TARGETOS=
+
+# source all the go environment variables
+. <(go env)
+if [[ ! -z $TARGETARCH ]]; then
+	export GOARCH=${TARGETARCH}
+fi
+if [[ ! -z $TARGETOS ]]; then
+	export GOOS=${TARGETOS}
+fi
+TMPDIR=$(mktemp -d)
+VERSION=$(./pgvacman --version | awk '{ print $2 }')
+RELNAME="pgvacman-${VERSION}-${GOOS}-${GOARCH}"
+RELDIR="${TMPDIR}/${RELNAME}"
+mkdir ${RELDIR}
+go build -o ${RELDIR}/
+cp -r examples ${RELDIR}
+cp -r README.md ${RELDIR}
+cp LICENSE ${RELDIR}
+go-licenses save --save_path ${RELDIR}/licenses github.com/jlucasdba/pgvacman
+OLDDIR="$PWD"
+cd ${TMPDIR}
+tar cvzf "${RELNAME}.tar.gz" ${RELNAME}
+cd "${OLDDIR}"
+cp "${TMPDIR}/${RELNAME}.tar.gz" .
+rm -rf ${TMPDIR}
